1734b9c1264651ac267296f85e2936b38d80b7ef
JenkmanShahin <Jenkmanshahin17@gmail.com>
2022-10-18 15:55:21 +0200

    done

--- farborakel/ViewController.swift
+++ farborakel/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background_button: UIButton!
+    
+    @IBOutlet weak var
+        circle_button: UIButton!
+    
+    @IBOutlet weak var circle: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        circle.layer.cornerRadius = 100
     }
-
+    
+    @IBAction func changebackground(_ sender: Any) {self.view.backgroundColor = randomcolor()
+    }
+    
+    @IBAction func changecirclecolor(_ sender: Any) {
+        circle.backgroundColor = randomcolor()
+    }
+    
+    func randomcolor() -> UIColor{
+            let red: CGFloat = CGFloat(drand48())
+            let green: CGFloat = CGFloat(drand48())
+            let blue: CGFloat = CGFloat(drand48())
+            return UIColor(red:red, green:green, blue:blue, alpha: 1.0)
+        }
 
 }
 
